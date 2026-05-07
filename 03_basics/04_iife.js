f092b9707760efad7910d277d903dcac5498f81b
SARVESH CHAUDHARY <141572008+sarveshrocks8@users.noreply.github.com>
2026-05-07 09:55:40 +0530

    Improve comment on IIFE usage
    Add a comment explaining the purpose of IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,5 +1,5 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+//most of the there aree pollution in global scope variables -> so to protect from this pollution we use iife function which executes immediately 
 
 (function chai(){
     // named IIFE
